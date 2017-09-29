428f4fd1dc9de8c2758970bac1fef6ff42af79e8
Eduarda Helena <dudinhahelenaa@gmail.com>
2017-09-29 10:41:47 -0300

    trab

---
+++ Assets/Editor/PotionSelectorDrawer.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System;
+public class Editor : MonoBehaviour {
+
+    [CustomPropertyDrawer(typeof(PotionSelectorAttribute))]
+    public class PotionSelectorDrawer : PropertyDrawer
+    {
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            int index = 0;
+            string[] names = new string[PotionDB.Instance.potions.Length];
+            int[] heals = new int[PotionDB.Instance.potions.Length];
+         
+
+            for (int i = 0; i < PotionDB.Instance.potions.Length; i++)
+            {
+                names[i] = PotionDB.Instance.potions[i].name;
+                heals[i] = PotionDB.Instance.potions[i].heal;
+
+               
+
+                if (names[i] == property.stringValue)
+                {
+                    index = i;
+                }
+
+            }
+
+            Rect popupRect = new Rect(position);
+            popupRect.xMax -= 64;
+
+            index = EditorGUI.Popup(popupRect, label.text, index, names);
+            property.stringValue = names[index];
+
+            Rect healRect = new Rect(position);
+            healRect.xMin = popupRect.xMax;
+
+            EditorGUI.IntField(healRect, heals[index]);
+
+            string heals1 = heals.ToString();
+            // index1 = EditorGUI.Label(position, index1, heals1);
+            //   property.stringValue = heals1[index1];
+          //  EditorGUILayout.TextField(heals[index].ToString());
+            //EditorGUILayout.TextField(heals[index].ToString());
+        }
+    }
+}
---
+++ Assets/Scripts/TestBehaviour.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+[System.AttributeUsage(System.AttributeTargets.Field)]
+public class PotionSelectorAttribute :PropertyAttribute
+{
+}
+
+public class TestBehaviour : MonoBehaviour
+{
+
+    [PotionSelector]
+    public string potionName;
+    public void Start()
+    {
+        for (int i = 0; i < PotionDB.Instance.potions.Length; i++)
+        {
+            if (PotionDB.Instance.potions[i].name == potionName)
+            {
+                Debug.Log(PotionDB.Instance.potions[i].heal);
+
+            }
+
+            // PotionDB.Instance pra acessar o "banco de dados" de pocoes
+            // Imprimir o quanto aquela potion recupera
+        }
+    }
+}
